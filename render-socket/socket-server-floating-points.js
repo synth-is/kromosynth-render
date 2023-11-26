ff159f5d4613ca32722f03883113055a30075f80
Björn Þór Jónsson <bangsi@bthj.is>
2023-11-26 01:24:15 +0100

    logging updates.

--- render-socket/socket-server-floating-points.js
+++ render-socket/socket-server-floating-points.js
@@ -28,6 +28,7 @@ wss.on("connection", async function connection(ws) {
   ws.on("message", async function incoming(message) {
     const messageParsed = JSON.parse(message);
     // console.log("received: %s", messageParsed);
+    console.log(`rendering sound from genome at duration ${messageParsed.duration} with noteDelta ${messageParsed.noteDelta} and velocity ${messageParsed.velocity}`);
     const {
       genomeString,
       duration,
